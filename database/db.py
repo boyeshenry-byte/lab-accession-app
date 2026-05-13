dc5081a1aa650eca2d277c67f381299982768134
Boyes <BOYESH@ccf.org>
2026-05-13 12:51:46 -0400

    feat add db.py for database creation

---
+++ database/db.py
@@ -0,0 +1,28 @@
+import sqlite3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_path = os.getenv('DB_PATH')
+schema_path = os.path.join(os.path.dirname(__file__), 'schema.sql')
+
+def get_db_connection(db_path):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db_schema(schema_path):
+    with open(schema_path, 'r') as f:
+        return f.read()
+
+def init_db(db_path):
+    conn = get_db_connection(db_path)
+    with conn:
+        schema = get_db_schema(schema_path)
+        conn.executescript(schema)
+    conn.close()
+
+if __name__ == '__main__':
+    print("starting")
+    init_db(db_path)
+    print(f"Database initialized at {db_path}") 
