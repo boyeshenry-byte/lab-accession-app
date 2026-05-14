766e8331cf48a3a838270ff331f7343789e0b249
Boyes <BOYESH@ccf.org>
2026-05-14 12:23:00 -0400

    feat: add naviagtion and new accession button

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class App(ctk.CTk):
     def create_widgets(self):
         self.home_frame = HomeFrame(self)
        
+    def show_frame(self, frame_class, **kwargs):
+        if self.current_frame:
+            self.current_frame.destroy()
+        self.current_frame = frame_class(self, **kwargs)
 
 if __name__ == "__main__":
     app = App()
--- ui/home.py
+++ ui/home.py
@@ -17,9 +17,27 @@ class HomeFrame(ctk.CTkFrame):
     def combobox_callback(self, choice):
         print(f"Selected: {choice}")
 
+    def new_accession(self):
+            study_name = self.combobox.get()
+            if study_name:
+                conn = get_db_connection(db_path)
+                study = conn.execute("SELECT study_id FROM studies WHERE study_name = ?", (study_name,)).fetchone()
+                conn.close()
+                if study:
+                    #self.master.show_frame(NewAccessionFrame, study_id=study['study_id'], study_name=study_name)
+                    print(f"Study ID: {study['study_id']}, Study Name: {study_name}")
+            else:
+                print("Please select a study before creating a new accession.")
+
     def create_widgets(self):
         self.label = ctk.CTkLabel(self, text="Home")
         self.label.pack(pady=12, padx=10)
 
         self.combobox = ctk.CTkComboBox(self, values=self.combobox_options, command=self.combobox_callback)
         self.combobox.pack(pady=12, padx=10)
+
+        self.new_accession_button = ctk.CTkButton(self, text="New Accession", command=self.new_accession)
+        self.new_accession_button.pack(pady=16, padx=10)
+
+    
+        
