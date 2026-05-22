d0a96b3449a890e1a0da3f2747d01126615df25c
Henry Boyes <boyeshenry@gmail.com>
2026-05-22 11:58:37 -0400

    refactor: remove redundant search accessions from home

--- ui/home.py
+++ ui/home.py
@@ -21,15 +21,15 @@ class HomeFrame(ctk.CTkFrame):
         self.new_accession_button = ctk.CTkButton(self, text="New Accession", command=self.new_accession)
         self.new_accession_button.pack(pady=16, padx=10)
 
-        self.patient_search_button = ctk.CTkButton(self, text="Search/Edit Accessions", command=self.search_accessions)
-        self.patient_search_button.pack(pady=16, padx=10)
+        #self.patient_search_button = ctk.CTkButton(self, text="Search/Edit Accessions", command=self.search_accessions)
+        #self.patient_search_button.pack(pady=16, padx=10)
 
         self.view_accession_button = ctk.CTkButton(self, text="View Accession Details", command=self.view_accession)
         self.view_accession_button.pack(pady=16, padx=10)
 
-    def search_accessions(self):
-        from ui.search_accession import SearchAccessionFrame
-        self.master.show_frame(SearchAccessionFrame)
+    #def search_accessions(self):
+        #from ui.search_accession import SearchAccessionFrame
+        #self.master.show_frame(SearchAccessionFrame)
 
     def view_accession(self):
         from ui.view import ViewAccessionFrame
